0dd0e689d44f6219115ad7e1e07817fa9c905767
Ivan Fedurin <iofedurin@gmail.com>
2022-08-18 17:20:03 +0400

    Update typings

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,13 @@ import { TextStyle } from '@pixi/text';
 
 export class HTMLText extends Sprite
 {
-    constructor(text?: string, style?: Partial<TextStyle>, canvas?: HTMLCanvasElement, cssStyle?: Record<string, string>);
+    constructor(
+      text?: string,
+      style?: Partial<TextStyle>,
+      canvas?: HTMLCanvasElement,
+      cssStyle?: Record<string, string>,
+      resolution?: number,
+    );
     readonly canvas: HTMLCanvasElement;
     readonly context: CanvasRenderingContext2D;
     text: string;
